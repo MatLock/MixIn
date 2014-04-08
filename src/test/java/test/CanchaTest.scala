904f13f61d51a0e93ba893e533fd45410c123233
MatLock <jfflores90@gmail.com>
2014-04-08 20:09:37 -0300

    agregado test

--- src/test/java/test/CanchaTest.scala
+++ src/test/java/test/CanchaTest.scala
@@ -1,3 +1,6 @@
+/**
+ * @author: MatLock 
+ */
 package test
 import org.scalatest.junit.JUnitRunner
 import org.junit.runner.RunWith
@@ -11,17 +14,17 @@ import scala.collection.mutable._
 @RunWith(classOf[JUnitRunner])
 class CanchaTest extends FlatSpec with ShouldMatchers {
   
-
+                  /**  SET UP!     */
 				val listaDeR =  Map[Int,ArrayBuffer[Int]]()
 				val canchaFutbol = new Futbol("futbol1",listaDeR) with Iluminado with ConTecho with conTribuna
 	            val club = new Club("El grillo")
 				club.canchas += canchaFutbol
-				//club.reservar("titular", 8, 20, "futbol1")
-				//val reserva = club.reservas find {a => a.titular.equals("titular")}
 				club.reservar("titular", 8, 20, "futbol1")
 
-	    
-		    "reservar from futbol1" should "be 1056" in {
+				
+				
+	        /** Test!! **/
+		    "El costo de futbol1 debe" should "ser  1056" in {
 			val reserva = club.reservas find {a => a.titular.equals("titular")}
 			println(reserva.get.costo)
 		    reserva.get.costo should equal(1056.0)
